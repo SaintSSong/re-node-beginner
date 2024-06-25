4aa459838480b5274c847fed48136f299fbfa56b
songsamuel <songsme2886@gmail.com>
2024-06-26 00:57:17 +0900

    feat: 상품 생성, 목록, 상세 조회 API 구현
    resolved #3, resolved #4, resolved #5

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -24,43 +24,49 @@ productsRouter.post('/products', async (req, res) => {
     .json({ status: 201, message: '상품 생성에 성공했습니다.', data: data });
 });
 // 상품 목록 조회(R)
-productsRouter.get('/products', (req, res) => {});
-// DB에서 조회하기(생성일시 기준 내림차순 정렬)
+productsRouter.get('/products', async (req, res) => {
+  // DB에서 조회하기(생성일시 기준 내림차순 정렬)
+  const data = await Product.find().sort({ createAt: 'desc' }).exec();
+  //.exec()는 에러가 났을때 어디서 났는지 알려주는 좋은 것! find 쓸 때 같이 쓰자.
 
-// 완료 메세지 반환하기
+  // 완료 메세지 반환하기
+  return res
+    .status(200)
+    .json({ status: 200, message: '상품 목록 조회에 성공했습니다.', data });
+});
 
 // 상품 상세 조회(R)
-productsRouter.get('/products/:id', (req, res) => {});
-// 상품 ID 파싱하기
+productsRouter.get('/products/:id', async (req, res) => {
+  // 상품 ID 파싱하기
+  // const id = req.params.id; <- 이걸
+  const { id } = req.params; /** 구조분해 할당으로 이렇게 만든 거다. */
 
-// DB에 조회하기
+  // DB에 조회하기
+  const data = await Product.findById(id).exec();
 
-// 완료 메세지 반환하기
+  // 완료 메세지 반환하기
+  return res
+    .status(200)
+    .json({ status: 200, message: '상품 상세 조회에 성공했습니다.', data });
+});
 
 // 상품 수정(U)
-productsRouter.put('/products/:id', (req, res) => {});
-// 상품 ID 파싱하기
-
-// 상품 수정 정보 파싱하기
-
-// DB에 조회하기(패스워드포함)
-
-// 비밀번호 일치 여부 확인
-
-// DB에 갱신하기
-
-// 완료 메세지 반환하기
+productsRouter.put('/products/:id', (req, res) => {
+  // 상품 ID 파싱하기
+  // 상품 수정 정보 파싱하기
+  // DB에 조회하기(패스워드포함)
+  // 비밀번호 일치 여부 확인
+  // DB에 갱신하기
+  // 완료 메세지 반환하기
+});
 
 // 상품 삭제(D)
-productsRouter.delete('/products/:id', (req, res) => {});
-// 상품 ID 파싱하기
-
-// DB에 조회하기(패스워드포함)
-
-// 비밀번호 일치 여부 확인
-
-// DB에서 삭제하기
-
-// 완료 메세지 반환하기
+productsRouter.delete('/products/:id', (req, res) => {
+  // 상품 ID 파싱하기
+  // DB에 조회하기(패스워드포함)
+  // 비밀번호 일치 여부 확인
+  // DB에서 삭제하기
+  // 완료 메세지 반환하기
+});
 
 export { productsRouter }; // 4번째 줄 만든 것을 내보낸다.
