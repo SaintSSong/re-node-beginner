8e94e4c08a0be3f4d7d89fa960df1e93e0725bd8
songsamuel <songsme2886@gmail.com>
2024-06-26 10:29:17 +0900

    유효성 검증 1/2

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,41 +1,49 @@
 import express from 'express';
 import { Product } from '../schemas/product.schema.js';
 
+import { createProductValidator } from '../middlewares/vaildators/create-product.validator.middleware.js';
+
 const productsRouter = express.Router();
 
 // 상품 생성(C)
-productsRouter.post('/products', async (req, res, next) => {
-  try {
-    // 상품 정보 파싱하기
-    const { name, description, manager, password } = req.body;
+productsRouter.post(
+  '/products',
+  createProductValidator,
+  async (req, res, next) => {
+    try {
+      // 상품 정보 파싱하기
+      const { name, description, manager, password } = req.body;
 
-    // DB에 조회하기(패스워드포함)
-    const existedProduct = await Product.findOne({ name }).exec();
+      // DB에 조회하기(패스워드포함)
+      const existedProduct = await Product.findOne({ name }).exec();
 
-    if (existedProduct) {
-      return res
-        .status(400)
-        .json({ status: 400, message: '이미 등록된 상품입니다.' });
+      if (existedProduct) {
+        return res
+          .status(400)
+          .json({ status: 400, message: '이미 등록된 상품입니다.' });
+      }
+
+      // DB에 저장하기 // 저장할 때는 스키마(44번째 줄 이름)에서 임포트해온 것을 가져온다.
+      const product = new Product({ name, description, manager, password });
+
+      let data = await product.save(); // product에 save된 결과가 data로 들어간다.
+
+      // 여기서 .toJSON()을 쓰는 이유는 .toJSON을 안쓰면
+      // 몽고DB에서 가져오는 원하지 않는 DB를 다 가져오기 때문에 그렇다.
+      // JSON으로 바꿔서 필요한 데이터만 가져와서 거기서 또 필요 없는 것을 뺀다.(password)
+      data = { ...data.toJSON(), password: undefined };
+
+      // 완료 메세지 반환하기
+      return res.status(201).json({
+        status: 201,
+        message: '상품 생성에 성공했습니다.',
+        data: data,
+      });
+    } catch (error) {
+      next(error);
     }
-
-    // DB에 저장하기 // 저장할 때는 스키마(44번째 줄 이름)에서 임포트해온 것을 가져온다.
-    const product = new Product({ name, description, manager, password });
-
-    let data = await product.save(); // product에 save된 결과가 data로 들어간다.
-
-    // 여기서 .toJSON()을 쓰는 이유는 .toJSON을 안쓰면
-    // 몽고DB에서 가져오는 원하지 않는 DB를 다 가져오기 때문에 그렇다.
-    // JSON으로 바꿔서 필요한 데이터만 가져와서 거기서 또 필요 없는 것을 뺀다.(password)
-    data = { ...data.toJSON(), password: undefined };
-
-    // 완료 메세지 반환하기
-    return res
-      .status(201)
-      .json({ status: 201, message: '상품 생성에 성공했습니다.', data: data });
-  } catch (error) {
-    next(error);
-  }
-});
+  },
+);
 
 // 상품 목록 조회(R)
 productsRouter.get('/products', async (req, res, next) => {
